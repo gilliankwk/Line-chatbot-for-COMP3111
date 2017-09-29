dd7df47fafc395f177ad380c668b656dd689c714
Gillian Kong <hillianxd@gmail.com>
2017-09-29 11:33:55 +0800

    Test #1

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -12,7 +12,38 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
-		return null;
+		String result = null;
+		
+		try {
+//			String username="programmer";
+//			String password="iamaprogrammer";
+//			String dbUrl="jdbc:postgresql://localhost:5432/chatbotDB";
+//
+//			Connection connection=DriverManager.getConnection(dbUrl,username,password);
+			Connection connection = getConnection();
+			PreparedStatement stmt=connection.prepareStatement(
+				"SELECT response FROM LINECHATBOT where keyword like concat('%', ?, '%')");
+		
+			stmt.setString(1,text);
+		
+			ResultSet rs=stmt.executeQuery();
+			while (rs.next()){
+				result = rs.getString(1);
+			}
+		
+			rs.close();
+			stmt.close();
+			connection.close();
+		
+		} 
+		catch (Exception e){
+			System.out.println(e);
+		}
+
+		if(result != null) {
+			return result;
+		}
+		throw new Exception("NOT FOUND");
 	}
 	
 	
